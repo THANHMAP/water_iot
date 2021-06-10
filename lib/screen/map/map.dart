87aa419d2296a01fa130cbdd68eac777cb3b2711
THANHMAP <nguyenthanh892009@gmail.com>
2021-06-10 21:06:58 +0700

    update UI map

--- lib/screen/map/map.dart
+++ lib/screen/map/map.dart
@@ -110,7 +110,7 @@ class _MapPageState extends State {
                 child: new Container(
                   child: new Column(
                     children: <Widget>[
-                      _buildCard2(),
+                      _buildCard(),
                     ],
                   ),
                 ),
@@ -122,7 +122,7 @@ class _MapPageState extends State {
     );
   }
 
-  Widget _buildCard2() {
+  Widget _buildCard() {
     return Padding(
       padding: const EdgeInsets.all(15.0),
       child: Card(
@@ -163,6 +163,10 @@ class _MapPageState extends State {
                 ),
               ),
 
+              Divider(
+                color: Colors.black,
+                height: 20,
+              ),
               Container(
                 width: double.infinity,
                 padding: EdgeInsets.symmetric(vertical: 10, horizontal: 0),
@@ -172,6 +176,22 @@ class _MapPageState extends State {
                     new Row(
                       children: [
                         Expanded(
+                          flex: 1,
+                          child: Container(
+                            child: Align(
+                              alignment: Alignment.topLeft,
+                              child: SvgPicture.asset(
+                                'assets/images/ic_place.svg',
+                                color: Color(0xFF556DD3),
+                                height: 20.0,
+                                width: 20.0,
+                                allowDrawingOutsideViewBox: true,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 8,
                           child: Container(
                             child: Text(
                               'Factory 1',
@@ -182,9 +202,6 @@ class _MapPageState extends State {
                                 fontWeight: FontWeight.bold,
                               ),
                             ),
-                            // child: Center(
-                            //
-                            // ),
                           ),
                         ),
                       ],
@@ -210,6 +227,11 @@ class _MapPageState extends State {
                     SizedBox(height: 18),
                   ],
                 ),
+              ),
+
+              Divider(
+                color: Colors.black,
+                height: 20,
               ),
               Container(
                 width: double.infinity,
@@ -220,9 +242,25 @@ class _MapPageState extends State {
                     new Row(
                       children: [
                         Expanded(
+                          flex: 1,
+                          child: Container(
+                            child: Align(
+                              alignment: Alignment.topLeft,
+                              child: SvgPicture.asset(
+                                'assets/images/ic_place.svg',
+                                color: Color(0xFF556DD3),
+                                height: 20.0,
+                                width: 20.0,
+                                allowDrawingOutsideViewBox: true,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 8,
                           child: Container(
                             child: Text(
-                              'Factory 2',
+                              'Factory 1',
                               style: TextStyle(
                                 color: Color(0xFF556DD3),
                                 fontFamily: 'OpenSans',
@@ -230,9 +268,6 @@ class _MapPageState extends State {
                                 fontWeight: FontWeight.bold,
                               ),
                             ),
-                            // child: Center(
-                            //
-                            // ),
                           ),
                         ),
                       ],
@@ -260,10 +295,72 @@ class _MapPageState extends State {
                 ),
               ),
 
+              Divider(
+                color: Colors.black,
+                height: 20,
+              ),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+                margin: EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+                child: Column(
+                  children: <Widget>[
+                    new Row(
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Container(
+                            child: Align(
+                              alignment: Alignment.topLeft,
+                              child: SvgPicture.asset(
+                                'assets/images/ic_place.svg',
+                                color: Color(0xFF556DD3),
+                                height: 20.0,
+                                width: 20.0,
+                                allowDrawingOutsideViewBox: true,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 8,
+                          child: Container(
+                            child: Text(
+                              'Factory 1',
+                              style: TextStyle(
+                                color: Color(0xFF556DD3),
+                                fontFamily: 'OpenSans',
+                                fontSize: 26.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    new Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            child: Text(
+                              'Address: Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet.',
+                              style: TextStyle(
+                                color: textDashboardColor,
+                                fontFamily: 'OpenSans',
+                                fontSize: 17.0,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 18),
+                  ],
+                ),
+              ),
 
-              // Row(
-
-              // ),
             ],
           ),
         ),
@@ -271,3 +368,219 @@ class _MapPageState extends State {
     );
   }
 }
+
+
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_svg/svg.dart';
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
+//
+// import '../../constants.dart';
+//
+// class MapPage extends StatefulWidget {
+//   @override
+//   State<StatefulWidget> createState() => _MapPageState();
+// }
+//
+// class _MapPageState extends State {
+//   static final LatLng _kMapCenter =
+//   LatLng(19.018255973653343, 72.84793849278007);
+//   static final CameraPosition _kInitialPosition =
+//   CameraPosition(target: _kMapCenter, zoom: 11.0, tilt: 0, bearing: 0);
+//   CameraPosition vegasPosition =
+//   CameraPosition(target: LatLng(36.0953103, -115.1992098), zoom: 10);
+//
+//   // Widget _buildGoogleMap(BuildContext context) {
+//   //   return Positioned(
+//   //     left: 50,
+//   //     right: 30,
+//   //     top: 100,
+//   //     bottom: 300,
+//   //     child: Container(
+//   //       child: GoogleMap(
+//   //         mapType: MapType.normal,
+//   //         // initialCameraPosition: vegasPosition,
+//   //       ),
+//   //     ),
+//   //   );
+//   // }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(
+//           "GOOGLE MAP",
+//           style: TextStyle(color: mTexHeadLoginColor),
+//         ),
+//         centerTitle: true,
+//         actions: <Widget>[
+//           IconButton(
+//             icon: SvgPicture.asset(
+//               'assets/images/ic_notification.svg',
+//               height: 20.0,
+//               width: 20.0,
+//               allowDrawingOutsideViewBox: true,
+//             ),
+//             onPressed: () {
+//               Navigator.pop(context);
+//               // ScaffoldMessenger.of(context).showSnackBar(
+//               //     const SnackBar(content: Text('This is a snackbar')));
+//             },
+//           ),
+//         ],
+//         leading: IconButton(
+//             icon: SvgPicture.asset(
+//               'assets/images/ic_back.svg',
+//               height: 20.0,
+//               width: 20.0,
+//               allowDrawingOutsideViewBox: true,
+//             ),
+//             onPressed: () {
+//               Navigator.pop(context);
+//               // Do something.
+//             }),
+//       ),
+//       body: Container(
+//         child: new Container(
+//           child: new Column(
+//             children: <Widget>[
+//               SingleChildScrollView(
+//                 child: new Container(
+//                   child: new Column(
+//                     children: <Widget>[
+//                       _buildMap(),
+//                       _buildItem(),
+//                       _buildItem(),
+//                     ],
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+//
+//   Widget _buildMap() {
+//     return Padding(
+//       padding: const EdgeInsets.all(15.0),
+//       child: Card(
+//         clipBehavior: Clip.antiAlias,
+//         elevation: 16,
+//         shape:
+//         RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+//         child: InkWell(
+//           onTap: () {},
+//           child: Column(
+//             mainAxisSize: MainAxisSize.min,
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: <Widget>[
+//               Container(
+//                 width: double.infinity,
+//                 padding: EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+//                 margin: EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+//                 child: Column(
+//                   children: <Widget>[
+//                     new Row(
+//                       children: [
+//                         Expanded(
+//                           child: Container(
+//                             height: 200,
+//                             width: MediaQuery.of(context).size.width,
+//                             child: GoogleMap(
+//                               initialCameraPosition: vegasPosition,
+//                               onMapCreated: (GoogleMapController controller) {},
+//                               myLocationButtonEnabled: false,
+//                               tiltGesturesEnabled: false,
+//                             ),
+//                           ),
+//                         ),
+//                       ],
+//                     ),
+//                     SizedBox(height: 13),
+//                   ],
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+//
+//   Widget _buildItem() {
+//     return Container(
+//       child: Column(
+//         children: <Widget>[
+//           Divider(
+//             color: Colors.black,
+//             height: 20,
+//           ),
+//           Container(
+//             width: double.infinity,
+//             padding: EdgeInsets.symmetric(vertical: 10, horizontal: 0),
+//             margin: EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+//             child: Column(
+//               children: <Widget>[
+//                 new Row(
+//                   children: [
+//                     Expanded(
+//                       flex: 1,
+//                       child: Container(
+//                         child: Align(
+//                           alignment: Alignment.topLeft,
+//                           child: SvgPicture.asset(
+//                             'assets/images/ic_place.svg',
+//                             color: Color(0xFF556DD3),
+//                             height: 20.0,
+//                             width: 20.0,
+//                             allowDrawingOutsideViewBox: true,
+//                           ),
+//                         ),
+//                       ),
+//                     ),
+//                     Expanded(
+//                       flex: 8,
+//                       child: Container(
+//                         child: Text(
+//                           'Factory 1',
+//                           style: TextStyle(
+//                             color: Color(0xFF556DD3),
+//                             fontFamily: 'OpenSans',
+//                             fontSize: 26.0,
+//                             fontWeight: FontWeight.bold,
+//                           ),
+//                         ),
+//                       ),
+//                     ),
+//                   ],
+//                 ),
+//                 SizedBox(height: 10),
+//                 new Row(
+//                   children: [
+//                     Expanded(
+//                       child: Container(
+//                         child: Text(
+//                           'Address: Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet.',
+//                           style: TextStyle(
+//                             color: textDashboardColor,
+//                             fontFamily: 'OpenSans',
+//                             fontSize: 17.0,
+//                             fontWeight: FontWeight.normal,
+//                           ),
+//                         ),
+//                       ),
+//                     ),
+//                   ],
+//                 ),
+//                 SizedBox(height: 18),
+//               ],
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
